61720363495d366370912b859c17b217288c0320
Gauthier <gauthier.michon@ynov.com>
2021-04-01 10:19:33 +0200

    Connexion API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Newtonsoft.Json;
 using BridgeMonitor.Models;
 
 namespace BridgeMonitor.Controllers
@@ -20,12 +22,30 @@ namespace BridgeMonitor.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var bridge = GetBridgeFromApi();
+            return View(bridge);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        private static List<BridgeModel> GetBridgeFromApi()
+        {
+            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
+            using (var client = new HttpClient())
+            {
+                //Interrogation de l'URL censée me retourner les données
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
+                //d'un flux de données vers des objets => Déserialisation
+                //d'objets vers un flux de données => Sérialisation
+                var result = JsonConvert.DeserializeObject<List<BridgeModel>>(stringResult.Result);
+                return result;
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
